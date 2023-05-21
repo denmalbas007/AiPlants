d8c5e572a41dd9abec006970bebee17769066306
lepyoshik <lepyoshik@gmail.com>
2023-05-21 10:13:14 +0300

    Button position changed

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,6 +3,7 @@ import telebot
 from telebot import types
 from simple_recsys import get_recs
 
+
 def areas(path):
     plants = pd.read_excel(path)
     plants['Ареалы произрастания'] = plants['Ареалы произрастания'].str.lstrip()
@@ -11,37 +12,28 @@ def areas(path):
     unique = df_exploded['col_split'].unique()
     return unique
 
-id_key = '6032665394:AAGJJW2ael1pY42fl-FKaTjp9qYj48DSC2M'
 
-# Создание экземпляра бота
+id_key = '6032665394:AAGJJW2ael1pY42fl-FKaTjp9qYj48DSC2M'
 bot = telebot.TeleBot(id_key)
 
-# Обработчик команды /start
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    # Создание списка значений для кнопок
     options = areas('Plants.xlsx')
-
-    # Создание объекта клавиатуры с кнопками
     keyboard = types.ReplyKeyboardMarkup(row_width=1, one_time_keyboard=True)
+    keyboard.add(types.KeyboardButton('Получить полную таблицу'))
     buttons = [types.KeyboardButton(option) for option in options]
     keyboard.add(*buttons)
-
-    keyboard.add(types.KeyboardButton('Получить полную таблицу'))
-
-    # Отправка приветственного сообщения с клавиатурой
     bot.send_message(chat_id=message.chat.id, text='Привет! Выбери ареал произрастания:', reply_markup=keyboard)
 
-# Обработчик текстовых сообщений
+
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def handle_text(message):
     if message.text == 'Получить полную таблицу':
         with open('Plants.xlsx', 'rb') as file:
-            bot.send_document(chat_id=message.chat.id, data=file)
+            bot.send_document(chat_id=message.chat.id, document=file)
     else:
         recs = get_recs(message.text)
-        columns = ['Название лекарственной культуры', 'В каких медицинских препаратах содержится, наименование', 'Ежегодная потребность лекарственного сырья, тонны', 'Timedelta_plant']
-        # Отправка сообщения с выбранным значением
         output = ''
         for k, v in recs.items():
             output += f'''{k + 1}. Название лекарственной культуры: {v['Название лекарственной культуры']}\n
@@ -51,5 +43,5 @@ def handle_text(message):
             Период сбора урожая, мес: {v['meantime_collect']}\n'''
         bot.send_message(chat_id=message.chat.id, text=f"Вы выбрали: {message.text}\nТоп 5 растений по данному ареалу:\n {output}")
 
-# Запуск бота
+
 bot.polling()
